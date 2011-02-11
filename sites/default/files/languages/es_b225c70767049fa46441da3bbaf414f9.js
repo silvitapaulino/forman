44922e72151a9723e0652119635d08e73f7e323a
alfonso <aheredia@mediaglobe.es>
2011-02-11 12:42:30 +0100

    Version inicial del Proyecto Aula 2.0

---
+++ sites/default/files/languages/es_b225c70767049fa46441da3bbaf414f9.js
@@ -0,0 +1,1 @@
+Drupal.locale = { 'pluralFormula': function($n) { return Number(($n!=1)); }, 'strings': { "The document already contains a teaser break. Do you want to proceed by removing it first?": "El documento ya contiene una marca de separación. ¿Quiere eliminarla?" } };
